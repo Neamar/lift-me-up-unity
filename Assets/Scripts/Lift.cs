6bae9d6db469efaa04ca3be46617803af55b0656
Matthieu Bacconnier <neamar@neamar.fr>
2017-10-02 18:22:28 +0100

    Lift on the right is moving!

---
+++ Assets/Scripts/Lift.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+	public float SPEED = 0.05f;
+	public Shaft shaft;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 currentPosition = this.transform.position;
+
+		float targetCoordinate = shaft.GetTargetFloorCoordinates ();
+
+		if (targetCoordinate > currentPosition.y) {
+			currentPosition.y += SPEED;
+		} else if (targetCoordinate < currentPosition.y) {
+			currentPosition.y -= SPEED;
+		}
+
+		if (Mathf.Abs (targetCoordinate - currentPosition.y) < SPEED) {
+			currentPosition.y = targetCoordinate;
+		}
+
+		this.transform.position = currentPosition;
+	}
+}
---
+++ Assets/Scripts/Shaft.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaft : MonoBehaviour
+{
+	public int floorHeight = 2;
+	private int numberOfFloors;
+
+	public int targetFloor = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		numberOfFloors = (int)this.transform.localScale.y / floorHeight;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnMouseUpAsButton ()
+	{
+		Vector3 worldCoordinates = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		targetFloor = (int)((worldCoordinates.y + transform.localScale.y / 2) / floorHeight);
+
+		Debug.Log ("New target floor: " + targetFloor);
+	}
+
+	public float GetTargetFloorCoordinates ()
+	{
+		return targetFloor * floorHeight - transform.localScale.y / 2 + floorHeight / 2;
+	}
+}
