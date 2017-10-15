ee6da79ff34fdde64a4268ee2f65b86e90285d4b
Matthieu Bacconnier <neamar@neamar.fr>
2017-10-15 11:44:09 +0100

    Added animation

--- Assets/Scripts/Lift.cs
+++ Assets/Scripts/Lift.cs
@@ -7,9 +7,11 @@ public class Lift : MonoBehaviour
 	public float SPEED = 0.05f;
 	public Shaft shaft;
 
+	private Animator animator;
+
 	void Start ()
 	{
-		
+		animator = GetComponent<Animator> ();
 	}
 
 	void Update ()
@@ -17,6 +19,10 @@ public class Lift : MonoBehaviour
 		Vector3 currentPosition = this.transform.position;
 
 		float targetCoordinate = shaft.GetTargetFloorCoordinates ();
+
+		if (targetCoordinate != currentPosition.y) {
+			animator.SetTrigger ("hasTargetFloor");
+		}
 
 		if (targetCoordinate > currentPosition.y) {
 			currentPosition.y += SPEED;
@@ -26,6 +32,7 @@ public class Lift : MonoBehaviour
 
 		if (Mathf.Abs (targetCoordinate - currentPosition.y) < SPEED) {
 			currentPosition.y = targetCoordinate;
+			animator.SetTrigger ("atTargetFloor");
 		}
 
 		this.transform.position = currentPosition;
