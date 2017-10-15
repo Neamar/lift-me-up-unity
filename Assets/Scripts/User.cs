2ac5a8c278d7cb47a92201c90a1cb599769a8d09
Matthieu Bacconnier <neamar@neamar.fr>
2017-10-15 13:03:40 +0100

    Added User script

--- Assets/Scripts/User.cs
+++ Assets/Scripts/User.cs
@@ -2,19 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Floor : MonoBehaviour
-{
-	public User[] UsersWaiting;
+public class User : MonoBehaviour {
 
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
+	void Update () {
 		
 	}
 }
