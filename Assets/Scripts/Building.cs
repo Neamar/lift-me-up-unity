a4bd96df48148f7bfbca5d57785c341b9b95e829
Matthieu Bacconnier <neamar@neamar.fr>
2017-10-22 19:58:29 +0100

    Starting Prefab strategy

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+	public GameObject Shaft;
+	public GameObject Floor;
+	public GameObject User;
+
 	public Floor[] floors;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		GameObject ground = Instantiate (Floor);
+		GameObject firstFloor = Instantiate (Floor);
 	}
 	
 	// Update is called once per frame
